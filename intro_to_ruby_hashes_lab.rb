5a920a0239d85093783d36303d87761a36bd57b9
Alex Farmer <farmale10@gmail.com>
2019-08-29 03:32:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,5 +57,6 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   fourth_hash = monopoly_with_third_tier
+  fourth_hash[:railroads][:names][:reading_railroad]
 
 end
