bd9f412e66e6001ccc1152c632beeb399ca14efe
Alex Farmer <farmale10@gmail.com>
2019-08-29 03:29:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,25 @@ def monopoly_with_third_tier
       :three_pieces_owned=>100,
       :four_pieces_owned=>200  
   }
-  third_hash[:railroads][:names] = {}
+  third_hash[:railroads][:names] = {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+
   third_hash
 end
 
