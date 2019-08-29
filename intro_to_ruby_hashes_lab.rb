4e887451b545a5b8852f3fc3d4af403dbe9d932e
Alex Farmer <farmale10@gmail.com>
2019-08-29 03:24:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,12 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   third_hash = monopoly_with_second_tier
-  third_hash[:railroads][:rent_in_dollars] = {}
+  third_hash[:railroads][:rent_in_dollars] = {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200  
+  }
   third_hash
 end
 
